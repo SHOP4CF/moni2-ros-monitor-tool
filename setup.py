30671fbd3da1e93bac1d5fdc9adc66d022edad2e
Rasmus Lunding Henriksen <rlh@teknologisk.dk>
2020-06-03 15:14:00 +0200

    use find_packages to find packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'moni2'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
