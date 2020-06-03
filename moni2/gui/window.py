a82b96f120f067b210629db95608a226e1ad03b4
Rasmus Lunding Henriksen <rlh@teknologisk.dk>
2020-06-03 16:24:01 +0200

    Tried out a DockingWidget

--- moni2/gui/window.py
+++ moni2/gui/window.py
@@ -1,10 +1,13 @@
 import logging
 from PyQt5.QtWidgets import (
     QMainWindow,
+    QDockWidget,
     QWidget,
     QVBoxLayout,
-    QLabel
+    QLabel,
+    QListWidget
 )
+from PyQt5.QtCore import Qt
 
 
 class MonitorWindow(QMainWindow):
@@ -13,15 +16,29 @@ class MonitorWindow(QMainWindow):
         super().__init__(parent)
         self.log = log
 
+        self.label = QLabel("hejsa")
+
+        self.init_components()
+        self.init_ui()
+
+    def init_components(self):
+        pass
+
+    def init_ui(self):
         self.setWindowTitle("Moni2")
         self.setGeometry(100, 100, 640, 480)
 
         widget = QWidget(self)
         layout = QVBoxLayout(widget)
         self.setCentralWidget(widget)
-
-        self.label = QLabel("hejsa")
         layout.addWidget(self.label)
+
+        dockWidget = QDockWidget('Dock test', self)
+        listWidget = QListWidget()
+        listWidget.addItem('Google')
+        listWidget.addItem('Apple')
+        dockWidget.setWidget(listWidget)
+        self.addDockWidget(Qt.RightDockWidgetArea, dockWidget)
 
         self.show()
 
