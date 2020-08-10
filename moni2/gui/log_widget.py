e73ea7e56669efbfd65c23acdc088384f803230e
Rasmus Lunding Henriksen <rlh@teknologisk.dk>
2020-08-10 15:20:18 +0200

    Extended the functionality of the log_widget.py

--- moni2/gui/log_widget.py
+++ moni2/gui/log_widget.py
@@ -1,4 +1,5 @@
 import logging
+import fnmatch
 from rcl_interfaces.msg import Log
 from typing import Optional
 from PyQt5.QtWidgets import (
@@ -7,9 +8,11 @@ from PyQt5.QtWidgets import (
     QListWidget,
     QMenu,
     QAction,
-    QActionGroup
+    QActionGroup,
+    QVBoxLayout,
+    QLineEdit,
 )
-from PyQt5.QtGui import QContextMenuEvent
+from PyQt5.QtGui import QContextMenuEvent, QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 
 
@@ -23,33 +26,65 @@ class LogWidget(QDockWidget):
     def __init__(self, log: logging.Logger, parent: Optional[QWidget] = None):
         super().__init__("Log widget", parent)
         self.log = log
-        self.log_level = logging.INFO
 
-        self.log_list = QListWidget(self)
+        self.log_level = logging.INFO
+        self.log_list = []
+        self.log_list_view = QListWidget()
+        self.filter_input = QLineEdit()
+        self.filter_text = ''
         self.init_components()
         self.init_ui()
         self.log.info("LogWidget initialized!")
 
     def init_components(self):
         self.log.info("Initializing components...")
-        self.log_list.setUniformItemSizes(True)
-        self.log_list.setAlternatingRowColors(True)
-        self.log_list.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.log_list_view.setUniformItemSizes(True)
+        self.log_list_view.setAlternatingRowColors(True)
+        self.log_list_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+        self.filter_input.textChanged.connect(self.on_filter_text_changed)
+        self.filter_input.setHidden(True)
+        self.filter_input.setClearButtonEnabled(True)
+        action = self.filter_input.addAction(QIcon.fromTheme('window-close'), QLineEdit.TrailingPosition)
+        action.triggered.connect(lambda _: self.filter_input.setHidden(True))
 
     def init_ui(self):
         self.log.info("Initializing UI...")
-        self.setWidget(self.log_list)
+        main_widget = QWidget(self)
+        main_layout = QVBoxLayout(main_widget)
+
+        main_layout.addWidget(self.filter_input)
+        main_layout.addWidget(self.log_list_view)
+
+        self.setWidget(main_widget)
 
     @pyqtSlot(str)
     def received_log(self, log: Log):
-        self.log_list.insertItem(0, f'[{self.LOG_LABEL[log.level]}] [{log.name}]: {log.msg}')
+        self.log_list.append(log)
+        if self.log_level <= log.level:
+            self.log_list_view.insertItem(0, f'[{self.LOG_LABEL[log.level]}] [{log.name}]: {log.msg}')
+
+    @pyqtSlot(str)
+    def on_filter_text_changed(self, text):
+        text = f'*{text}*' if text else "*"
+        for row in range(self.log_list_view.count()):
+            item = self.log_list_view.item(row)
+            item.setHidden(not fnmatch.fnmatch(item.text(), text))
+        self.filter_text = text
 
     def contextMenuEvent(self, event: QContextMenuEvent) -> None:
+        # Build menu
         menu = QMenu(self)
+        search_action = QAction(QIcon.fromTheme('edit-find'), "Search", menu)
+        menu.addAction(search_action)
+        clear_action = QAction(QIcon.fromTheme('edit-clear'), "Clear list", menu)
+        menu.addAction(clear_action)
+        menu.addSeparator()
+
         group = QActionGroup(self)
         group.setExclusive(True)
         for key, val in self.LOG_LABEL.items():
-            action = QAction(self)
+            action = QAction(menu)
             action.setCheckable(True)
             action.setText(val)
             action.setData(key)
@@ -57,6 +92,21 @@ class LogWidget(QDockWidget):
             if key == self.log_level:
                 action.setChecked(True)
             menu.addAction(action)
+
+        # Handle result
         action = menu.exec(event.globalPos())
         if action:
-            self.log_level = action.data()
+            if action == clear_action:
+                self.log.info("Clear list")
+                self.log_list.clear()
+                self.log_list_view.clear()
+            elif action == search_action:
+                self.log.info("Search")
+                self.filter_input.setHidden(False)
+            else:
+                if self.log_level != action.data():
+                    self.log_level = action.data()
+                    self.log_list_view.clear()
+                    for log in self.log_list:
+                        if self.log_level <= log.level:
+                            self.log_list_view.insertItem(0, f'[{self.LOG_LABEL[log.level]}] [{log.name}]: {log.msg}')
--- test/test_log_widget.py
+++ test/test_log_widget.py
@@ -21,13 +21,36 @@ class TestLogWidget:
         print("Teardown")
         assert self.log_widget
 
+    @staticmethod
+    def create_log(text: str, level: int) -> Log:
+        log = Log()
+        log.msg = text
+        log.level = level
+        return log
+
+    def assert_list(self, total_length: int, view_length: int):
+        assert len(self.log_widget.log_list) == total_length, f"log_list should have {total_length} elements"
+        assert self.log_widget.log_list_view.count() == view_length, f"log_list_view should have {view_length} elements"
+
     def test_init(self):
         assert self.log_widget
 
     def test_received_log(self):
-        assert self.log_widget.log_list.count() == 0, "List of logs should be empty"
-        log = Log()
-        log.msg = "Hello"
-        log.level = 10
-        self.log_widget.received_log(log)
-        assert self.log_widget.log_list.count() == 1, "List of logs should have 1 entry"
+        self.assert_list(0, 0)
+
+        self.log_widget.received_log(self.create_log("Hello", logging.INFO))
+        self.assert_list(1, 1)
+
+    def test_filter(self):
+        self.assert_list(0, 0)
+
+        self.log_widget.received_log(self.create_log("Hello", logging.INFO))
+        self.log_widget.received_log(self.create_log("Davs", logging.INFO))
+        self.assert_list(2, 2)
+
+        self.log_widget.on_filter_text_changed("av")
+        self.assert_list(2, 2)
+        hidden = 0
+        for row in range(self.log_widget.log_list_view.count()):
+            hidden += self.log_widget.log_list_view.item(row).isHidden()
+        assert hidden == 1, f"1 out of 2 items should be hidden"
