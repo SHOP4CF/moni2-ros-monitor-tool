7282594aa84462e12612ed4b401930279f0ebb67
Rasmus Lunding Henriksen <rlh@teknologisk.dk>
2020-08-14 15:12:48 +0200

    Implemented widgets to show a list of nodes and detailed information

--- moni2/gui/__init__.py
+++ moni2/gui/__init__.py
@@ -1,4 +1,6 @@
 from moni2.gui.window import MonitorWindow
 from moni2.gui.log_widget import LogWidget
+from moni2.gui.node_widget import NodeWidget
+from moni2.gui.node_detail_widget import NodeDetailWidget
 
-__all__ = ['MonitorWindow', 'LogWidget']
+__all__ = ['MonitorWindow', 'LogWidget', 'NodeWidget', 'NodeDetailWidget']
