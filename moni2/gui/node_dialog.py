d6f3d3cabf17a0dac9f0fa0d1eafd8a98597667c
Rasmus Lunding Henriksen <rlh@teknologisk.dk>
2020-08-25 10:38:34 +0200

    Implemented a Dialog to select a list of nodes

---
+++ moni2/gui/node_dialog.py
@@ -0,0 +1,75 @@
+import logging
+from typing import Optional
+from PyQt5.QtWidgets import (
+    QWidget,
+    QDialog,
+    QDialogButtonBox,
+    QVBoxLayout,
+    QHBoxLayout,
+    QListWidget,
+    QAbstractItemView,
+    QLineEdit,
+    QPushButton,
+)
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from moni2.node_info import NodeInfo
+
+
+class EditNodeListDialog(QDialog):
+    node_list_updated = pyqtSignal(list)
+
+    def __init__(self, log: logging.Logger, parent: Optional[QWidget] = None):
+        super().__init__(parent)
+        self.log = log
+
+        self.nodes: [str] = []
+        self.node_list = QListWidget()
+        self.new_node_input: QLineEdit = QLineEdit()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("Add node")
+
+        self.node_list.setSelectionMode(QAbstractItemView.MultiSelection)
+
+        self.new_node_input.setPlaceholderText("Other node")
+        new_node_button = QPushButton("Add node")
+        new_node_button.clicked.connect(self._manual_add_node)
+        new_node_layout = QHBoxLayout()
+        new_node_layout.addWidget(self.new_node_input)
+        new_node_layout.addWidget(new_node_button)
+
+        button = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        button_box = QDialogButtonBox(button)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.node_list)
+        layout.addStretch(2)
+        layout.addLayout(new_node_layout)
+        layout.addSpacing(20)
+        layout.addWidget(button_box)
+        self.setLayout(layout)
+
+    def set_nodes(self, nodes: [str]):
+        self.nodes = nodes
+        for node in nodes:
+            self.node_list.addItem(node)
+
+    def accept(self) -> None:
+        nodes = [node.text() for node in self.node_list.selectedItems()]
+        self.log.info(f"Nodes: {nodes}")
+        self.node_list_updated.emit(nodes)
+        super().accept()
+
+    @pyqtSlot(NodeInfo)
+    def node_updated(self, node: NodeInfo):
+        if node.name not in self.nodes:
+            self.nodes.append(node.name)
+            self.node_list.addItem(node.name)
+
+    def _manual_add_node(self):
+        node = self.new_node_input.text()
+        self.node_list.addItem(node)
